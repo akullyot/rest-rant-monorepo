e1ecf1df7478d56e6513291cc23b0eb6c06f69d3
akullyot <akullyot@gmail.com>
2023-12-09 15:01:27 -0600

    added signup password hashing and login authentication controller

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -5,7 +5,7 @@ export const CurrentUser = createContext()
 
 function CurrentUserProvider({ children }){
 
-    const [currentUser, setCurrentUser] = useState(null)
+    const [currentUser, setCurrentUser] = useState(null);
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
