d52fc9f0a195d7faf975bc58fbe5a91530411ac8
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2023-12-12 22:13:55 -0600

    implemented sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -9,15 +9,33 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({
         where: { email: req.body.email }
     });
-    console.log(user)
-
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ 
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        //Set this user into your session
+        req.session.userId = user.userId;
         res.json({ user })
     }
-})
+});
+
+//Purpose: return the currently logged in user
+router.get('/profile', async(req,res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        });
+        console.log(user)
+        res.json(user)
+    } catch (error) {
+        //send over nothing
+        res.json(null);
+    }
+});
+
+
 
 module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,27 @@
 // Modules and Globals
 require('dotenv').config();
-const express    = require('express');
-const bodyParser = require('body-parser');
-const cors       = require('cors')
-const app        = express();
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const cors          = require('cors')
+const cookieSession = require('cookie-session');
+const app           = express();
 
 // Express Settings
-app.use(cors());
+//Instantiate your cookie session
+app.use(cookieSession({
+    name  :'session',
+    keys  : [process.env.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000
+}));
+//allowing our front end to include credentials in its fetc
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//Add in express sessions
 
 // Controllers & Routes
 
