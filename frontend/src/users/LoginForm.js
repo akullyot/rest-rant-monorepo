5cd22fe6b52c1614abc85c6a40f321e269c5608e
Annie Ullyot <50632137+akullyot@users.noreply.github.com>
2023-12-12 22:14:21 -0600

    implemented sessions

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -14,6 +14,7 @@ function LoginForm() {
     async function handleSubmit(e) {
         const response = await fetch(`http://localhost:5000/authentication/`, {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
